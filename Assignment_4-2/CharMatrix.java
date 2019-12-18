a0812b9741c98878628193a46ff628a1bbe500b8
JeffZhang314 <56049849+JeffZhang314@users.noreply.github.com>
2019-12-18 12:18:55 -0800

    Add files via upload

--- Assignment_4-2/CharMatrix.java
+++ Assignment_4-2/CharMatrix.java
@@ -10,7 +10,7 @@ public class CharMatrix {
      * Constructor: creates a grid with dimensions rows, cols,
      * and fills it with spaces
      */
-    public static void CharMatrix(int rows, int cols) {
+    public CharMatrix(int rows, int cols) {
         m = new char [rows] [cols];
         fillRect(0, 0, rows - 1, cols - 1, ' ');
     }
@@ -19,7 +19,7 @@ public class CharMatrix {
      * Constructor: creates a grid with dimensions rows , cols ,
      * and fills it with the fill  character
      */
-    public static void CharMatrix(int rows, int cols, char fill) {
+    public CharMatrix(int rows, int cols, char fill) {
         m = new char [rows] [cols];
         fillRect(0, 0, rows - 1, cols - 1, fill);
     }
@@ -27,28 +27,28 @@ public class CharMatrix {
     /*
      * Returns the number of rows in grid
      */
-    public static int numRows() {
+    public int numRows() {
         return m.length; //
     }
 
     /*
      * Returns the number of columns in grid
      */
-    public static int numCols() {
+    public int numCols() {
         return m [0].length; //
     }
 
     /*
      * Returns the character at the indexes [row, col]
      */
-    public static char charAt(int row, int col){
+    public char charAt(int row, int col){
         return m [row] [col];
     }
 
     /*
      * Sets the character at index [row, col] to ch
      */
-    public static void setCharAt(int row, int col, char ch) {
+    public void setCharAt(int row, int col, char ch) {
         m [row] [col] = ch;
     }
 
@@ -56,8 +56,8 @@ public class CharMatrix {
      * Returns true if the character at row, col is a SPACE,
      * false otherwise
      */
-    public static boolean isEmpty(int row, int col) {
-        return m [row] [col] == ' ';
+    public boolean isEmpty(int row, int col) {
+        return charAt(row, col) == ' ';
     }
 
     /*
@@ -65,7 +65,7 @@ public class CharMatrix {
      * row0, col0 is the upper left corner and row1, col1 is the
      * lower right corner of the rectangle.
      */
-    public static void fillRect(int row0, int col0, int row1, int col1, char fill) {
+    public void fillRect(int row0, int col0, int row1, int col1, char fill) {
         for (int r = row0; r <= row1; r ++) {
             for (int c = col0; c <= col1; c ++) {
                 m [r] [c] = fill;
@@ -78,14 +78,14 @@ public class CharMatrix {
      * row0, col0 is the upper left corner and row1, col1 is the
      * lower right corner of the rectangle.
      */
-    public static void clearRect(int row0, int col0, int row1, int col1) {
+    public void clearRect(int row0, int col0, int row1, int col1) {
         fillRect(row0, col0, row1, col1, ' ');
     }
 
     /*
      * Returns the count of all non-SPACE characters in row.
      */
-    public static int countInRow(int row) {
+    public int countInRow(int row) {
         int output = 0;
         for (int c = 0; c < numCols(); c ++) {
             if (charAt(row, c) != ' ') {
@@ -98,7 +98,7 @@ public class CharMatrix {
     /*
      * Returns the count of all non-SPACE characters in col.
      */
-    public static int countInCol(int col) {
+    public int countInCol(int col) {
         int output = 0;
         for (int r = 0; r < numRows(); r ++) {
             if (charAt(r, col) != ' ') {
@@ -111,7 +111,7 @@ public class CharMatrix {
     /*
      * Displays the contents of the matrix m
      */
-    public static void display () {
+    public void display () {
         for (int r = 0; r < m.length; r++) {
             for (int c = 0; c < m[0].length; c++) {
                 System.out.print(m[r][c]);
@@ -126,10 +126,24 @@ public class CharMatrix {
 
         //program in your test cases here. Invoke methods from the
         //object called matrix, like this:
-
+        
+        matrix = new CharMatrix(5, 8);
         matrix.display();
+        matrix = new CharMatrix(4, 6, 'C');
+        matrix.display();
+        System.out.println(matrix.numRows());
+        System.out.println(matrix.numCols());
+        System.out.println(matrix.charAt(3, 2));
+        matrix.setCharAt(3, 0, ' ');
+        matrix.display();
+        System.out.println(matrix.isEmpty(3, 0));
+        System.out.println(matrix.isEmpty(3, 2));
+        matrix.fillRect(1, 2, 2, 5, ':');
+        matrix.display();
+        matrix.clearRect(1, 4, 3, 4);
+        System.out.println(matrix.countInCol(3));
+        System.out.println(matrix.countInRow(1));
     }
-
 }
 
 
